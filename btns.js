b51825c3aadc6558b877853fb7a73b3e7441de16
Ricardonovais1 <ricardonovais1@gmail.com>
2023-04-13 14:21:18 -0300

    teste botão verde

--- btns.js
+++ btns.js
@@ -1,18 +1,27 @@
 // Tela 1 - Pinball
 // Botão verde:
 
-const greenBtnUp = document.getElementById('green-up');
-const greenBtnDw = document.getElementById('green-dw');
+const greenBtnUp = './img/BTN_Launch-UP.png';
+const greenBtnDw = './img/BTN_Launch-DW.png';
 const areaGreen = document.querySelector('.area-green');
+const greenBtn = document.getElementById('green-btn');
 
-areaGreen.addEventListener('click', () => {
-    greenBtnUp.classList.add('hide');
-    greenBtnDw.classList.remove('hide');
-    setTimeout(() => {
-        greenBtnUp.classList.remove('hide');
-        greenBtnDw.classList.add('hide');
-    }, 300)
+areaGreen.addEventListener('touchstart', () => {
+    greenBtn.style.backgroundImage = `url(${greenBtnUp})`
 });
+
+areaGreen.addEventListener('touchend', () => {
+    greenBtn.style.backgroundImage = `url(${greenBtnDw})`
+});
+
+// areaGreen.addEventListener('click', () => {
+//     greenBtnUp.classList.toggle('hide');
+//     greenBtnDw.classList.toggle('hide');
+//     setTimeout(() => {
+//         greenBtnUp.classList.remove('hide');
+//         greenBtnDw.classList.add('hide');
+//     }, 300)
+// });
 
 // Botão roxo esquerdo:
 
@@ -20,14 +29,14 @@ const purpleLeftUp = document.getElementById('left-up');
 const purpleLeftDw = document.getElementById('left-dw');
 const areaPurpleLeft = document.querySelector('.area-purple-left');
 
-areaPurpleLeft.addEventListener('click', () => {
-    purpleLeftUp.classList.add('hide');
-    purpleLeftDw.classList.remove('hide');
-    setTimeout(() => {
-        purpleLeftUp.classList.remove('hide');
-        purpleLeftDw.classList.add('hide');
-    }, 300);
-});
+// areaPurpleLeft.addEventListener('click', () => {
+//     purpleLeftUp.classList.toggle('hide');
+//     purpleLeftDw.classList.toggle('hide');
+//     setTimeout(() => {
+//         purpleLeftUp.classList.remove('hide');
+//         purpleLeftDw.classList.add('hide');
+//     }, 300);
+// });
 
 // Botão roxo direito: 
 
@@ -35,14 +44,14 @@ const purpleRightUp = document.getElementById('right-up');
 const purpleRightDw = document.getElementById('right-dw');
 const areaPurpleRight = document.querySelector('.area-purple-right');
 
-areaPurpleRight.addEventListener('click', () => {
-    purpleRightUp.classList.add('hide');
-    purpleRightDw.classList.remove('hide');
-    setTimeout(() => {
-        purpleRightUp.classList.remove('hide');
-        purpleRightDw.classList.add('hide');
-    }, 300);
-});
+// areaPurpleRight.addEventListener('click', () => {
+//     purpleRightUp.classList.toggle('hide');
+//     purpleRightDw.classList.toggle('hide');
+//     setTimeout(() => {
+//         purpleRightUp.classList.remove('hide');
+//         purpleRightDw.classList.add('hide');
+//     }, 300);
+// });
 
 // Tela 2 - Confirmar
 
@@ -59,10 +68,12 @@ nicknameInput.addEventListener('input', () => {
 });
 
 confirmArea.addEventListener('click', () => {
-    confirmUp.classList.add('hide');
-    confirmDw.classList.remove('hide');
-    setTimeout(() => {
-        confirmUp.classList.remove('hide');
-        confirmDw.classList.add('hide');
-    }, 300);
+    if (confirmDw.classList.contains('hide')) {
+        confirmUp.classList.add('hide');
+        confirmDw.classList.remove('hide');
+        setTimeout(() => {
+            confirmUp.classList.remove('hide');
+            confirmDw.classList.add('hide');
+        }, 300);
+    }
 });
